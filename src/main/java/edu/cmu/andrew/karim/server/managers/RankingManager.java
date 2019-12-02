6b36ee9e919911974f944d29725f3bf9056b94ca
emily <0918277686>
2019-12-01 20:32:40 -0800

    Ranking list for search page v2

--- src/main/java/edu/cmu/andrew/karim/server/managers/RankingManager.java
+++ src/main/java/edu/cmu/andrew/karim/server/managers/RankingManager.java
@@ -135,8 +135,96 @@ public class RankingManager extends Manager{
                     address += "," + activityAddressDoc.getString("state");
                     // Get coordinates of Activity Provider
 //                    System.out.println("address:"+address);
+//                    String key = "AIzaSyB7smrK4e9AABXv1cLCosoTkArFjpm_Z0k";
+//                    String destination= location;
+//                    URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+address+"&destinations="+destination+"&key="+key);
+//                    System.out.println("url:"+url.toString());
+//                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//                    con.setRequestMethod("GET");
+//                    con.setRequestProperty("Content-Type", "application/json");
+//                    int status = con.getResponseCode();
+//                    System.out.println("Status code: "+status);
+//                    BufferedReader in = new BufferedReader(
+//                            new InputStreamReader(con.getInputStream()));
+//                    String inputLine;
+//                    StringBuffer content = new StringBuffer();
+
+//                    while ((inputLine = in.readLine()) != null) {
+//                        content.append(inputLine);
+                    }
+//                    String[] parser1 = content.toString().split(":");
+//                    int count = 0;
+//                    for (String s: parser1){
+//                        System.out.println(activityId+":"+s);
+//                        count++;
+//                        if (count%13 == 8){
+//                            String[] distanceString = parser1[count].split(" ");
+//                            int distanceValue = Integer.parseInt(distanceString[0]);
+//                            Ranking ranking = new Ranking(
+//                                rankingDoc.getString("activityId").toString(),
+//                                rankingDoc.getString("activityName").toString(),
+//                                rankingDoc.getString("activityProviderId"),
+//                                rankingDoc.getString("effectiveDate"),
+//                                rankingDoc.getString("endDate"),
+//                                rankingDoc.getString("activityCategory"),
+//                                rankingDoc.getString("description"),
+//                                rankingDoc.getString("photo"),
+//                                rankingDoc.getDouble("price"),
+//                                rankingDoc.getString("currency"),
+//                                rankingDoc.getString("publishStatus"),
+//                                distanceValue
+//                            );
+//                            rankingList.add(ranking);
+//                            System.out.println(ranking.getDistance());
+//                        }
+                    }
+//                    in.close();
+//                    con.disconnect();
+//                }
+//            }
+            return rankingList;
+        } catch(Exception e){
+            throw handleException("Get Ranking List", e);
+        }
+    }
+
+    public ArrayList<Ranking> calculateDistance(String location) throws AppException {
+        try{
+
+            // Return a list
+            ArrayList<Ranking> rankingList = new ArrayList<>();
+            FindIterable<Document> rankingDocs = activityCollection.find();
+            for(Document rankingDoc: rankingDocs) {
+                Ranking ranking = new Ranking(
+                        rankingDoc.getString("activityId").toString(),
+                        rankingDoc.getString("activityName").toString(),
+                        rankingDoc.getString("activityProviderId"),
+                        rankingDoc.getString("effectiveDate"),
+                        rankingDoc.getString("endDate"),
+                        rankingDoc.getString("activityCategory"),
+                        rankingDoc.getString("description"),
+                        rankingDoc.getString("photo"),
+                        rankingDoc.getDouble("price"),
+                        rankingDoc.getString("currency"),
+                        rankingDoc.getString("publishStatus"),
+                        0,
+                        rankingDoc.getString("avgRating")
+                );
+                rankingList.add(ranking);
+
+
+                // Get Activity and Activity Provider Address
+//                System.out.println(rankingDoc.getString("activityProviderId"));
+                String address="";
+                Document activityAddressDoc = activityProviderCollection.find(eq("activityProviderId", rankingDoc.getString("activityProviderId"))).first();
+                if (activityAddressDoc != null) {
+                    address = activityAddressDoc.getString("address1");
+                    address += "," + activityAddressDoc.getString("city");
+                    address += "," + activityAddressDoc.getString("state");
+                    // Get coordinates of Activity Provider
+//                    System.out.println("address:"+address);
                     String key = "AIzaSyB7smrK4e9AABXv1cLCosoTkArFjpm_Z0k";
-                    String destination= "San+Jose,CA";
+                    String destination= location;
                     URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+address+"&destinations="+destination+"&key="+key);
 //                    System.out.println("url:"+url.toString());
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -155,7 +243,7 @@ public class RankingManager extends Manager{
                     String[] parser1 = content.toString().split(":");
                     int count = 0;
                     for (String s: parser1){
-                        System.out.println(activityId+":"+s);
+                        //System.out.println(activityId+":"+s);
                         count++;
 //                        if (count%13 == 8){
 //                            String[] distanceString = parser1[count].split(" ");
